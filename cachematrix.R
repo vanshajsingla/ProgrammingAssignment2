fc075e5ad39c8588ecc7600d87314119c3dcf7c3
vanshajsingla <vanshajrnv2002@gmail.com>
2022-08-22 12:42:52 +0530

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The first function, makeCacheMatric creates a special "matrix", 
+## which is really a list containing functions to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse (mat)
+## get the value of the inverse (mat)
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
+  inv <- NULL                               # Create NULL invmat
+  set <- function(y){                       # A function that uses a dull
+    x <<- y                                 # var to store the matrix and
+    inv <<- NULL                            # the state of invmat
   }
-  get <- function() x
+  get <- function() x                       # function to return matrix
   setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
+  getInverse <- function() inv              # function to get inverse of mat
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
-}
+}                                           # list returned with above subsets
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
+  inv <- x$getInverse()                 #get the inverse of the matrix
+  if(!is.null(inv)){                    #if it's not null then return it
     message("getting cached data")
     return(inv)
   }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+  data <- x$get()                       #assign to data the cached (non-inverted) matrix
+  inv <- solve(data)                    #solve (invert the matrix)
+  x$setInverse(inv)                     #set-back to list
+  inv                                   #return result
 }
